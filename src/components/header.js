e0847890e9c204f8298f28fa45c95355133d635e
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2024-09-24 14:11:13 -0400

    Setup

---
+++ src/components/header.js
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header className="bg-white text-black p-4">
+            <nav className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <div className="text-2xl font-bold">
+                    <Link href="/">
+                        Corporation de développement de Schefferville
+                    </Link>
+                </div>
+
+                {/* Navigation Links */}
+                <ul className="flex space-x-6">
+                    <li>
+                        <Link href="/">
+                            Infolettre
+                        </Link>
+                    </li>
+                    <li>
+                        <a href={"https://ville-schefferville.ca/"} target={"_blank"}>
+                            Ville de Schefferville
+                        </a>
+                    </li>
+                    <li>
+                        <Link href="/services">
+                            Nous joindre
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">
+                            En
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
