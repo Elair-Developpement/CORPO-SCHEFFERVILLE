fad592c9dcad1000a54f6a75034d4bb6e63321b1
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2024-10-09 23:37:54 -0400

    fixe eslint

--- src/components/home/serviceCard.js
+++ src/components/home/serviceCard.js
@@ -5,9 +5,11 @@ export default function ServiceCard({ title, description, path }) {
     <div className="flex flex-col items-start space-y-7 max-w-3xl">
       <h1 className="text-3xl text-left text-orange_1">{title}</h1>
       <p className="text-left">{description}</p>
-      <button className="self-center bg-green_1 hover:bg-white hover:text-green_1 hover:border-green_1 text-white font-bold py-3 px-5 border-2 rounded">
-        <Link href={path}>En savoir plus</Link>
-      </button>
+      <Link href={path}>
+        <button className="self-center bg-green_1 hover:bg-white hover:text-green_1 hover:border-green_1 text-white font-bold py-3 px-5 border-2 rounded">
+          En savoir plus
+        </button>
+      </Link>
     </div>
   );
 }
