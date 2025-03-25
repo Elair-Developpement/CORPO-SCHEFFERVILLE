c5ce7631ec0600d1c48cdef37e9dc5804a726dad
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-03-25 00:19:19 -0400

    Implémenté la page de connexion, ne redirige pas encore è la bonne page après connexion

--- src/app/(management)/login/page.js
+++ src/app/(management)/login/page.js
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import PageTitle from "@/components/common/pageTitle";
+import LoginForm from "@/components/auth/loginForm";
 
 export default function Login() {
   return (
-    <main className={"container mx-auto min-h-[calc(100vh-249.27px)]"}>
-      <Button variant="destructive">Bouton</Button>
+    <main
+      className={
+        "container mx-auto min-h-[calc(100vh-249.27px)] flex flex-col items-center justify-center"
+      }
+    >
+      <div className="w-full max-w-md">
+        <PageTitle title={"Connexion Admin"} />
+        <LoginForm />
+      </div>
     </main>
   );
 }
