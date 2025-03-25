018c3e15354d5d0c54cabaa48d41c3aff8a64ea0
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-03-25 01:37:06 -0400

    Modifié middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,58 +1,12 @@
-import { createServerClient } from "@supabase/ssr";
-import { NextResponse } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
-const protectedRoutes = ["/admin"];
+export async function middleware(request) {
+  return await updateSession(request);
+}
 
-/**
- * @param {import('next/server').NextRequest} request
- */
-export const middleware = async (request) => {
-  let supabaseResponse = NextResponse.next({
-    request,
-  });
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value),
-          );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options),
-          );
-        },
-      },
-    },
-  );
-
-  const pathname = request.nextUrl.pathname;
-
-  const isProtectedRoute = protectedRoutes.includes(pathname);
-
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
-  if (isProtectedRoute && (!user || error)) {
-    const redirectUrl = new URL("/login", request.url);
-    redirectUrl.searchParams.set("redirect", pathname);
-    return NextResponse.redirect(redirectUrl);
-  }
-
-  return supabaseResponse;
-};
-
-// Routes middleware should not run on
+// Chemins è exclure du middleware.
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
