5dfe56b4e83feb868f016a2f9e1a530078a24b33
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-03-26 16:32:14 -0400

    la page vie communautaire prends maintenant ses données de la bd

--- src/app/communal-life/page.js
+++ src/app/communal-life/page.js
@@ -2,73 +2,77 @@ import PageTitleAndDescription from "@/components/common/pageTitleAndDescription
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Communal_life() {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const { data: ongoing } = await supabase
-        .from("members")
-        .select("*")
-        .eq("category", "en-cours");
+  const { data: ongoing } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("category", "en-cours");
 
-    const { data: equipment } = await supabase
-        .from("members")
-        .select("*")
-        .eq("category", "equipements-disponibles");
+  const { data: equipment } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("category", "equipements-disponibles");
 
-    const { data: upcoming } = await supabase
-        .from("members")
-        .select("*")
-        .eq("category", "en-developpement");
+  const { data: upcoming } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("category", "en-developpement");
 
-    return (
-        <main className={"container mx-auto min-h-[calc(100vh-249.27px)] flex flex-col"}>
-            <PageTitleAndDescription
-                title={"Loisir, vie communautaire et culturelle, saines habitudes"}
-                description={
-                    "Nous souhaitons enrichir l’offre aux citoyens en matière de loisirs, de sport, de plein\n" +
-                    "air, d’activités culturelles et communautaires et de saines habitudes de vie. Des\n" +
-                    "activités sont en cours, peut-être souhaitez-vous démarrer un projet ou encore vous\n" +
-                    "informer sur les équipements disponibles et en développement."
-                }
-            />
-            <button
-                className={
-                    "self-center bg-green_1 hover:bg-white hover:text-green_1 hover:border-green_1 text-white font-bold py-3 px-5 border-2 rounded"
-                }
-            >
-                Démarrer un projet
-            </button>
-            <div className={"flex flex-col py-5"}>
-                <div className={"flex bg-orange_2 p-2 text-white"}>
-                    <div className={"flex w-full justify-between"}>
-                        <div className={"flex text-xl font-bold"}>Activités en cours</div>
-                        <div className={"flex-col text-right"}>
-                            {ongoing.map(activity => (
-                                <div key={activity.id}>{activity.name}</div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className={"flex p-2"}>
-                    <div className={"flex w-full justify-between"}>
-                        <div className={"flex text-xl font-bold"}>Équipements disponibles</div>
-                        <div className={"flex-col text-right"}>
-                            {equipment.map(activity => (
-                                <div key={activity.id}>{activity.name}</div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className={"flex bg-orange_2 p-2 text-white"}>
-                    <div className={"flex w-full justify-between"}>
-                        <div className={"flex text-xl font-bold"}>En développement</div>
-                        <div className={"flex-col text-right"}>
-                            {upcoming.map(activity => (
-                                <div key={activity.id}>{activity.name}</div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main
+      className={"container mx-auto min-h-[calc(100vh-249.27px)] flex flex-col"}
+    >
+      <PageTitleAndDescription
+        title={"Loisir, vie communautaire et culturelle, saines habitudes"}
+        description={
+          "Nous souhaitons enrichir l’offre aux citoyens en matière de loisirs, de sport, de plein\n" +
+          "air, d’activités culturelles et communautaires et de saines habitudes de vie. Des\n" +
+          "activités sont en cours, peut-être souhaitez-vous démarrer un projet ou encore vous\n" +
+          "informer sur les équipements disponibles et en développement."
+        }
+      />
+      <button
+        className={
+          "self-center bg-green_1 hover:bg-white hover:text-green_1 hover:border-green_1 text-white font-bold py-3 px-5 border-2 rounded"
+        }
+      >
+        Démarrer un projet
+      </button>
+      <div className={"flex flex-col py-5"}>
+        <div className={"flex bg-orange_2 p-2 text-white"}>
+          <div className={"flex w-full justify-between"}>
+            <div className={"flex text-xl font-bold"}>Activités en cours</div>
+            <div className={"flex-col text-right"}>
+              {ongoing?.map((activity) => (
+                <div key={activity.id}>{activity.name}</div>
+              ))}
             </div>
-        </main>
-    );
+          </div>
+        </div>
+        <div className={"flex p-2"}>
+          <div className={"flex w-full justify-between"}>
+            <div className={"flex text-xl font-bold"}>
+              Équipements disponibles
+            </div>
+            <div className={"flex-col text-right"}>
+              {equipment?.map((activity) => (
+                <div key={activity.id}>{activity.name}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={"flex bg-orange_2 p-2 text-white"}>
+          <div className={"flex w-full justify-between"}>
+            <div className={"flex text-xl font-bold"}>En développement</div>
+            <div className={"flex-col text-right"}>
+              {upcoming?.map((activity) => (
+                <div key={activity.id}>{activity.name}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
