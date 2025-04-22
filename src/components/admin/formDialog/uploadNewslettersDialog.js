825c73b216cd36db920c6156efbe4c25730d6dec
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-04-22 16:54:35 -0400

    fix typo

--- src/components/admin/formDialog/uploadNewslettersDialog.js
+++ src/components/admin/formDialog/uploadNewslettersDialog.js
@@ -28,7 +28,6 @@ export default function UploadNewslettersDialog() {
       setUploadStatus("Error uploading file: " + error.message);
     } else {
       setUploadStatus("File uploaded successfully!");
-      refre;
     }
   };
 
