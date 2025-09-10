bfb1765bd0e912addbfd4b6423a624fccbe55c13
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-09-10 12:39:44 -0400

    Ajusté formattage de la page d'habitation TODO: La carte s'Affiche au dessus du header

--- src/app/housing/page.jsx
+++ src/app/housing/page.jsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 import { useTranslations } from "next-intl";
 import { useState, useEffect, useTransition } from "react";
 
-import {createClient} from "@/lib/supabase/client";
+import { createClient } from "@/lib/supabase/client";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import PageTitleAndDescription from "@/components/common/pageTitleAndDescription";
@@ -14,14 +14,14 @@ const CentralLeafletMap = dynamic(
   () => import("@/components/map/centralLeafletMap"),
   {
     ssr: false,
-  },
+  }
 );
 
 const IndustrialLeafletMap = dynamic(
   () => import("@/components/map/industrialLeafletMap"),
   {
     ssr: false,
-  },
+  }
 );
 
 export default function Housing() {
@@ -35,8 +35,16 @@ export default function Housing() {
     const supabase = createClient();
 
     startTransition(async () => {
-      const { data: residentialData, error: residentialError } = await supabase.from("land").select("*").eq("residential", true).eq("active", true);
-      const {data: industrialData, error: industrialError} = await supabase.from("land").select("*").eq("residential", false).eq("active", true);
+      const { data: residentialData, error: residentialError } = await supabase
+        .from("land")
+        .select("*")
+        .eq("residential", true)
+        .eq("active", true);
+      const { data: industrialData, error: industrialError } = await supabase
+        .from("land")
+        .select("*")
+        .eq("residential", false)
+        .eq("active", true);
 
       // TODO: Ajouter error handling
 
@@ -47,19 +55,24 @@ export default function Housing() {
       if (industrialData) {
         setIndustrialProperties(industrialData);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <main
-      className={"block max-md:max-w-screen md:container md:mx-auto min-h-[calc(100vh-249.27px)]"}
+      className={
+        "block p-2 md:p-0 max-md:max-w-screen md:container md:mx-auto mt-[5rem] md:mt-0 min-h-[calc(100vh-249.27px)]"
+      }
     >
       <PageTitleAndDescription
         title={t("housing-alt")}
         description={t("page-text")}
       />
-      <div className="flex items-center space-x-2">
-        <Label htmlFor="map-toggle" className="text-lg font-semibold text-green_1">
+      <div className="flex items-center mt-2 space-x-2">
+        <Label
+          htmlFor="map-toggle"
+          className="text-lg font-semibold text-green_1"
+        >
           {t("industrial-sector")}
         </Label>
         <Switch
@@ -68,14 +81,26 @@ export default function Housing() {
           onCheckedChange={(checked) => setIsResidentialMap(checked)}
           className="data-[state=checked]:bg-orange_1 data-[state=unchecked]:bg-green_1"
         />
-        <Label htmlFor="map-toggle" className="text-lg font-semibold text-orange_1">
+        <Label
+          htmlFor="map-toggle"
+          className="text-lg font-semibold text-orange_1"
+        >
           {t("central-sector")}
         </Label>
       </div>
-      <div className={"flex flex-grow h-[60rem] w-full mb-7 z-0 rounded-2xl overflow-hidden"}>
-        {!isPending && (
-            isResidentialMap ? <CentralLeafletMap centralPropertiesProp={centralProperties} /> : <IndustrialLeafletMap industrialPropertiesProp={industrialProperties} />
-        )}
+      <div
+        className={
+          "flex flex-grow h-[30rem] md:h-[60rem] w-full md:mb-7 z-0 rounded-2xl overflow-hidden"
+        }
+      >
+        {!isPending &&
+          (isResidentialMap ? (
+            <CentralLeafletMap centralPropertiesProp={centralProperties} />
+          ) : (
+            <IndustrialLeafletMap
+              industrialPropertiesProp={industrialProperties}
+            />
+          ))}
       </div>
     </main>
   );
--- src/components/common/pageTitleAndDescription.js
+++ src/components/common/pageTitleAndDescription.js
@@ -1,6 +1,6 @@
 export default function PageTitleAndDescription({ title, description }) {
   return (
-    <div className="flex flex-col container py-5 space-y-4">
+    <div className="flex flex-col container md:py-5 space-y-2 md:space-y-4">
       <h1 className="text-3xl text-left text-orange_1 justify-center font-bold">
         {title}
       </h1>
